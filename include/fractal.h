a0ad1a3601eedc2b49ee07106484623a89b529aa
Marcel Goh <marcel.goh@mail.mcgill.ca>
2019-05-04 18:06:12 +0200

    Mandelbrot first draft

--- include/fractal.h
+++ include/fractal.h
@@ -8,7 +8,7 @@
 typedef long double complex ld_complex_t;
 
 /* outputs a colour given a number of iterations */
-SDL_Color colour_iters(int num_iters);
+SDL_Color colour_iters(unsigned int num_iters);
 
 /* fills a buffer representing 1/16 of the screen with colours */
 void mandelbrot(ld_complex_t top, ld_complex_t bottom, struct buffer_t *buf);
