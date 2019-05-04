f00f85f48f29394bc65578cc06b526087a5fafff
Marc-André Brochu <marcandrebrochu@gmail.com>
2019-05-04 18:29:50 +0200

    Test line

--- src/main.c
+++ src/main.c
@@ -7,29 +7,63 @@
 #define WINDOW_WIDTH 1000
 #define WINDOW_HEIGHT 600
 
-#define HORIZONTAL_SCREEN_REGIONS 4
-#define VERTICAL_SCREEN_REGIONS 4
+#define HORIZONTAL_REGIONS 4
+#define VERTICAL_REGIONS 4
 
 void fractal_worker(ld_complex_t region_top, ld_complex_t region_bot);
 
+// TODO: put that in another file or something
+void set_pixel(SDL_Surface *surface, int x, int y, Uint32 value) {
+    
+    if (x >= surface->w || y >= surface->h) {
+        printf("Error: trying to set pixel outside of surface bounds!\n");
+        return;
+    }
+
+    Uint32 *p = (Uint32 *)surface->pixels + y * surface->w + x;
+    *p = value;
+}
+
 int main() {
 
-    SDL_Event event;
-    SDL_Window *window;
-    SDL_Renderer *renderer;
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("Error initializing SDL: %s\n", SDL_GetError());
+        exit(EXIT_FAILURE);
+    }
 
-    SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_CreateWindowAndRenderer(WINDOW_WIDTH, WINDOW_HEIGHT, 0, &window, &renderer);
+    SDL_Window *window;
+    window = SDL_CreateWindow("Mattoni: Free Fractals for Everyone lol",
+        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+        WINDOW_WIDTH, WINDOW_HEIGHT,
+        SDL_WINDOW_SHOWN
+    );
+
+    if (window == NULL) {
+        printf("Error creating window: %s\n", SDL_GetError());
+        goto bail_window;
+    }
+
+    SDL_Surface *screen_surface; // The main surface on which everything is drawn.
+    screen_surface = SDL_GetWindowSurface(window);
+    SDL_FillRect(screen_surface, NULL, SDL_MapRGB(screen_surface->format, 0, 0, 0)); // TODO: remove this. this only fill the surface with black but once we get the workers going it wont be needed.
+
+    SDL_LockSurface(screen_surface);
+    set_pixel(screen_surface, 100, 100, 0xffffff);
+    printf("%x\n", SDL_MapRGB(screen_surface->format, 255, 255, 255));
+    for (int i = 0; i < WINDOW_WIDTH; i++) {
+        set_pixel(screen_surface, i, i, SDL_MapRGB(screen_surface->format, 255, 0, 0));
+    }
+    SDL_UnlockSurface(screen_surface);
 
     ld_complex_t viewport_top = CMPLXL(-2.0, 2.0);
     ld_complex_t viewport_bot = CMPLXL(2.0, -2.0);
 
     // For each region of the screen, create a fractal worker. Each worker will compute the part of
     // the fractal living in the region of the screen it is assigned by putting colors into a buffer.
-    long double region_w = (creall(viewport_bot) - creall(viewport_top)) / HORIZONTAL_SCREEN_REGIONS;
-    long double region_h = (cimagl(viewport_top) - cimagl(viewport_bot)) / VERTICAL_SCREEN_REGIONS;
-    for (int i = 0; i < HORIZONTAL_SCREEN_REGIONS; i++) {
-        for (int j = 0; j < VERTICAL_SCREEN_REGIONS; j++) {
+    long double region_w = (creall(viewport_bot) - creall(viewport_top)) / HORIZONTAL_REGIONS;
+    long double region_h = (cimagl(viewport_top) - cimagl(viewport_bot)) / VERTICAL_REGIONS;
+    for (int i = 0; i < HORIZONTAL_REGIONS; i++) {
+        for (int j = 0; j < VERTICAL_REGIONS; j++) {
 
             // 'Top' is top-left while 'bottom' ('bot') is bottom-right.
             ld_complex_t region_top = CMPLXL(i * region_w, j * region_h);
@@ -39,23 +73,17 @@ int main() {
         }
     }
 
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-    for (int i=0; i<1000; ++i) {
-        SDL_Color col = colour_iters(i);
-        SDL_SetRenderDrawColor(renderer, col.r, col.g, col.b, 0);
-        for (int j=0; j<WINDOW_HEIGHT; ++j) {
-            SDL_RenderDrawPoint(renderer, i, j+WINDOW_HEIGHT);
-        }
-    }
-    SDL_RenderPresent(renderer);
+    SDL_UpdateWindowSurface(window);
 
+    SDL_Event event;
     while (1) {
         if (SDL_PollEvent(&event) && event.type == SDL_QUIT) {
             break;
         }
     }
-    SDL_DestroyRenderer(renderer);
+
     SDL_DestroyWindow(window);
+    bail_window:
     SDL_Quit;
 
     return EXIT_SUCCESS;
@@ -67,7 +95,12 @@ void fractal_worker(ld_complex_t region_top, ld_complex_t region_bot) {
     // 3. Map the buffer to the SDL buffer.
     // 4. Delete buffer.
     // 5. Die.
-    struct buffer_t *buffer = make_buffer(/* we need to have the size of the region in pixels */);
+//    struct buffer_t *buffer = make_buffer(/* we need to have the size of the region in pixels */);
+//    SDL_Surface *region_surface = SDL_CreateRGBSurfaceFrom(
+//        (void *)buffer->colors,
+//        /* int width, int height */, 8
+//    );
+//
     return;
 }
 
